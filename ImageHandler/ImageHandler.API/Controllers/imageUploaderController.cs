61442b3ebdab4d0fd23556b5401bdc77b00f397f
Waqas Siddiqui <waqas.siddiqui@ntm.eu>
2017-12-07 10:14:55 +0100

    Checkin to merge

---
+++ ImageHandler/ImageHandler.API/Controllers/imageUploaderController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Web;
+using System.Web.Helpers;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace ImageHandler.API.Controllers
+{
+	[EnableCors(origins: "http://localhost:4200,http://127.0.0.1:4200", headers: "*", methods: "*")]
+	[RoutePrefix("api/imageUploader")]
+	public class imageUploaderController
+	{
+		/// <summary>
+		/// Get user applications
+		/// </summary>
+		/// <returns></returns>
+		[HttpGet]
+		[Route("test")]
+		public HttpResponseMessage UserApplications()
+		{
+			return Request.CreateResponse(HttpStatusCode.OK, "Test");
+		}
+	}
+}
