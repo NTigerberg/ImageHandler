24b214e9f372453e0d1024456f45609e4031a882
Waqas Siddiqui <waqas.siddiqui@ntm.eu>
2017-12-07 10:37:46 +0100

    Use image controller to upload files from angular.

--- ImageHandler/ImageHandler.API/Controllers/imageController.cs
+++ ImageHandler/ImageHandler.API/Controllers/imageController.cs
@@ -5,25 +5,25 @@ using System.Net;
 using System.Net.Http;
 using System.Text.RegularExpressions;
 using System.Web;
-using System.Web.Helpers;
 using System.Web.Http;
 using System.Web.Http.Cors;
 
 namespace ImageHandler.API.Controllers
 {
 	[EnableCors(origins: "http://localhost:4200,http://127.0.0.1:4200", headers: "*", methods: "*")]
-	[RoutePrefix("api/imageUploader")]
-	public class imageUploaderController
+	[RoutePrefix("api/image")]
+	public class ImageController
 	{
 		/// <summary>
 		/// Get user applications
 		/// </summary>
 		/// <returns></returns>
 		[HttpGet]
-		[Route("test")]
-		public HttpResponseMessage UserApplications()
+		[Route("upload")]
+		public string Upload()
 		{
-			return Request.CreateResponse(HttpStatusCode.OK, "Test");
+			return "Test";
+			//return Request.CreateResponse(HttpStatusCode.OK, "test");
 		}
 	}
 }
