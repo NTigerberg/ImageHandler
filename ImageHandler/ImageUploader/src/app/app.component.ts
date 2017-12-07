ebf0c8941b93de402a5c0758dec6c984d55de93f
Waqas Siddiqui <waqas.siddiqui@ntm.eu>
2017-12-07 16:23:45 +0100

    Get upload response from server after upload.

--- ImageHandler/ImageUploader/src/app/app.component.ts
+++ ImageHandler/ImageUploader/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Headers, Http } from '@angular/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  constructor(public http: Http	) {
+
+  }
+
   title = 'Image Uploader';
+  public url = 'http://localhost:35195/api/image/upload';
+
+  onUploadFinished($event) {
+    debugger;
+    console.log($event + 'Finished');
+  }
+  // uploadImage() {
+  //   return this.http.get(url).toPromise()
+  //   .then(response => response.json() as T);
+  // }
 }
